a85e7a4e0bcd03a56d223cea385af995fe009c3d
decoejz <decoejz@gmail.com>
2018-03-29 19:24:50 -0300

    not pronta

--- portas_logicas/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
+++ portas_logicas/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
@@ -1,5 +1,22 @@
-	package br.pro.hashi.ensino.desagil.rafaelogic.model;
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
 public class NotGate extends Gate{
+	//private Emitter[] emitters;
+	private NandGate nand = new NandGate();
 	
+	//@Override
+	public void NotGate(NandGate nand) {
+		this.nand = nand;
+	}
+
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		NotGate.this.nand.connect(0, emitter);
+		NotGate.this.nand.connect(1, emitter);
+	}
+
+	@Override
+	public boolean read() {
+		return nand.read();
+	}
 }
